2a953e88e3be0f6cceed673c7307cbc29acaa3d2
Rakshit Verma <r.verma@ubiquedigital.com>
2026-04-21 00:07:22 +0530

    Backend connected

--- Backend/main.py
+++ Backend/main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 # CORS (temporary open)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://resume-ai-analyzer-navy.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
